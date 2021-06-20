7a55a35e0793162cd94372a522737a4994704f00
venkatasai dholendar reddy <60260834+dholendar14@users.noreply.github.com>
2021-06-20 13:59:49 +0530

    made some changes

--- main.py
+++ main.py
@@ -10,40 +10,37 @@ app = Flask(__name__)
 
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin"
 
-def Cowin_api(pincode,vaccine,date):
+def Cowin_api(pincode,vaccine,new_date):
     response = requests.get(URL,
         params={
             "pincode" : pincode,
-            "date" : date,
+            "date" : new_date,
             "vaccine" : vaccine,
             "age" : "45"
     })
     data = response.json()
-    counter = 0
-    print("\t\tADDRESS\t\t\t\tAVAILABLE SLOTS")
+    l = []
     for i in range(0,len(data['sessions'])):
         if(data['sessions'][i]['available_capacity'] > 0 and data['sessions'][i]['min_age_limit'] <= 45):
-            print(str(data['sessions'][i]['address']) +","+str(data['sessions'][i]['available_capacity'])+","+str(data['sessions'][i]['available_capacity_dose1'])+","+str(data['sessions'][i]['available_capacity_dose1']))
-            return(str(data['sessions'][i]['address']) +","+str(data['sessions'][i]['available_capacity'])+","+str(data['sessions'][i]['available_capacity_dose1'])+","+str(data['sessions'][i]['available_capacity_dose1']))
-
+            res = (str(data['sessions'][i]['address']),str(data['sessions'][i]['available_capacity']),str(data['sessions'][i]['available_capacity_dose1']),str(data['sessions'][i]['available_capacity_dose2']))
+            l.append(res)
+        print(l)
+    return l
 
 
 @app.route('/',methods= ["GET","POST"])
 def index():
-    result = []
     if request.method == "POST":
         vaccine = request.form["Vaccine"]
         pincode = request.form["pin_code"]
         date = request.form["Date"]
         new_date = re.sub(r'(\d{4})-(\d{1,2})-(\d{1,2})', '\\3-\\2-\\1', date)
         result = Cowin_api(pincode,vaccine,new_date)
-        l = result.split(",")
-        print(l[0])
-        return render_template("index.html",address = l[0], dose = l[1])
+        return render_template("index.html",res = result)
     else:
         return render_template("index.html")
         
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(use_reloader = True,debug=True)
    
