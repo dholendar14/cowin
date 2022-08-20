33419b5d9434f2673ab1be0174db1e958df27f58
dholendar14 <dholendar1427@gmail.com>
2022-08-20 17:56:38 +0530

    Final Commit

--- app.py
+++ app.py
@@ -10,22 +10,23 @@ app = Flask(__name__)
 
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin"
 
+headings = ("Address","Available Vaccine", "First Dose", "Second Dose")
 def Cowin_api(pincode,vaccine,new_date):
     response = requests.get(URL,
         params={
             "pincode" : pincode,
             "date" : new_date,
             "vaccine" : vaccine,
-            "age" : "45"
+            "age" : "18"
     })
+    
     data = response.json()
     l = []
     for i in range(0,len(data['sessions'])):
         if(data['sessions'][i]['available_capacity'] > 0 and data['sessions'][i]['min_age_limit'] <= 45):
             res = (str(data['sessions'][i]['address']),str(data['sessions'][i]['available_capacity']),str(data['sessions'][i]['available_capacity_dose1']),str(data['sessions'][i]['available_capacity_dose2']))
             l.append(res)
-        print(l)
-    return l
+    return tuple(l)
 
 
 @app.route('/',methods= ["GET","POST"])
@@ -36,7 +37,7 @@ def index():
         date = request.form["Date"]
         new_date = re.sub(r'(\d{4})-(\d{1,2})-(\d{1,2})', '\\3-\\2-\\1', date)
         result = Cowin_api(pincode,vaccine,new_date)
-        return render_template("index.html",res = result)
+        return render_template("index.html",heading =headings, res = result)
     else:
         return render_template("index.html")
         
